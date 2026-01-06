39a64b5dbdd4d2b6b3ce7e7aee074f012fd6d123
Farah Abuassi <farahabuassi16@gmail.com>
2026-01-06 15:34:35 +0200

    feat : connecvt to railway Mysql env vars

--- src/db.php
+++ src/db.php
@@ -1,12 +1,17 @@
 <?php
-$host = "db";      // اسم service
-$user = "news_user";
-$pass = "news_pass";
-$db = "news_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("DB Connection failed: " . $conn->connect_error);
 }
+
+// echo "DB Connected Successfully";
+
 ?>
